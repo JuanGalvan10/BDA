f13d83e9b750f61b2914f76bcf9b94a557611fff
JuanGalvan10 <148394092+JuanGalvan10@users.noreply.github.com>
2024-11-20 14:25:37 -0600

    Create cliente.py

---
+++ python/models/cliente.py
@@ -0,0 +1,52 @@
+from database import mysql
+import MySQLdb.cursors
+import hashlib
+
+class Cliente:
+    @staticmethod
+    def get_all():
+        cur = mysql.connection.cursor()
+        cur.callproc('mostrarUsuarios')
+        users = cur.fetchall()
+        cur.close()
+        return users
+    
+    @staticmethod
+    def insert(nombre,apellidos,correo, telefono, direccion, puntos, password, nombre_usuario):
+        # Insertar el User
+        cur = mysql.connection.cursor()
+        cur.callproc('registrarUsuario', (nombre_usuario, password, 'cliente'))
+        mysql.connection.commit()
+        
+        # Obtener el ID del User recién insertado
+        id_usuario = cur.lastrowid
+
+        # Insertar detalles del User
+        cur.callproc('insertarCliente', (id_usuario, nombre, apellidos, telefono, correo, puntos, direccion))      
+        mysql.connection.commit()
+        cur.close()
+    
+    @staticmethod
+    def get_by_id(idUser):
+        cur = mysql.connection.cursor()
+        cur.callproc('obtenerUser(%s)', (idUser,))
+        User = cur.fetchone()
+        cur.close()
+        return User
+    
+    @staticmethod
+    def update(nombre,apellidos,correo, telefono, direccion, puntos):
+        cur = mysql.connection.cursor()
+        cur.callproc("actualizaCliente(%s,%s,%s,%s,%s,%s)", (nombre,apellidos,correo, telefono, direccion, puntos))
+        mysql.connection.commit()
+        cur.close()
+
+    @staticmethod
+    def delete(idCliente):
+        cur = mysql.connection.cursor()
+        cur.callproc('eliminarCliente(%s)', (idCliente,))
+        mysql.connection.commit()
+        cur.close()
+
+
+
