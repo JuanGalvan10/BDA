d0d89f478b16b030488ce49a6dd2cb79d78d0dad
JuanGalvan10 <148394092+JuanGalvan10@users.noreply.github.com>
2024-11-20 14:26:22 -0600

    Create reserva.py

---
+++ python/models/reserva.py
@@ -0,0 +1,42 @@
+from database import mysql
+import MySQLdb.cursors
+import hashlib
+
+class Reserva:
+    @staticmethod
+    def get_all():
+        cur = mysql.connection.cursor()
+        cur.callproc('mostrarReservas')
+        reservas = cur.fetchall()
+        cur.close()
+        return reservas
+    
+    @staticmethod
+    def get_by_id(empresa_id):
+        cur = mysql.connection.cursor()
+        cur.callproc('obtenerReserva(%s)', (empresa_id,))
+        reserva = cur.fetchone()
+        cur.close()
+        return reserva
+    
+    @staticmethod
+    def get_by_cliente(idUsuario):
+        cur = mysql.connection.cursor()
+        cur.callproc('obtenerReservasCliente(%s)', (idUsuario,))
+        reservas = cur.fetchall()
+        cur.close()
+        return reservas
+
+    @staticmethod
+    def insert(nombre, imagen_URL, precio, descripcion, categoria):
+        cur = mysql.connection.cursor()
+        cur.callproc("nuevoReserva(%s,%s,%s,%s,%s)", (nombre, imagen_URL, precio, descripcion, categoria))
+        mysql.connection.commit()
+        cur.close()
+
+    @staticmethod
+    def delete(empresa_id):
+        cur = mysql.connection.cursor()
+        cur.callproc('eliminarReserva(%s)', (empresa_id,))
+        mysql.connection.commit()
+        cur.close()
