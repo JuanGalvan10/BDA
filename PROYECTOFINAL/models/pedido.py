98f117ddbcf9bf83be09221bd43b4f08c6960258
Juan Galvan <juan.galvann@udem.edu>
2024-11-24 17:00:18 -0600

    Cambios

---
+++ PROYECTOFINAL/models/pedido.py
@@ -0,0 +1,61 @@
+from database import mysql
+import MySQLdb.cursors
+import hashlib
+
+class Pedido:
+    @staticmethod
+    def get_all():
+        cur = mysql.connection.cursor()
+        cur.callproc('mostrarPedidos')
+        pedidos = cur.fetchall()
+        cur.close()
+        return pedidos
+    
+    @staticmethod
+    def insert(productos,cantidades,id_cliente):
+        cur = mysql.connection.cursor()
+        try:
+            # Insertar el pedido
+            cur.callproc('insertarPedido', (id_cliente,))
+            mysql.connection.commit()
+            
+            # Obtener el ID del pedido recién insertado
+            id_pedido = cur.lastrowid
+
+            # Insertar detalles del pedido
+            for i, id_producto in enumerate(productos):
+                cantidad = cantidades[i]
+                cur.callproc('insertarDetallePedido', (id_pedido, id_producto, cantidad))      
+            mysql.connection.commit()
+            cur.close()
+            return True, 'Pedido exitoso'
+
+        except MySQLdb.OperationalError as e:
+            cur.close()
+            return False, str(e)
+
+    @staticmethod
+    def get_by_id(idPedido):
+        cur = mysql.connection.cursor()
+        cur.callproc('obtenerPedido(%s)', (idPedido,))
+        pedido = cur.fetchone()
+        cur.close()
+        return pedido
+    
+    @staticmethod
+    def get_by_cliente(idCliente):
+        cur = mysql.connection.cursor()
+        cur.callproc('obtenerPedidosCliente(%s)', (idCliente,))
+        pedidos = cur.fetchall()
+        cur.close()
+        return pedidos
+
+    @staticmethod
+    def delete(idPedido):
+        cur = mysql.connection.cursor()
+        cur.callproc('eliminarPedido(%s)', (idPedido,))
+        mysql.connection.commit()
+        cur.close()
+
+
+
